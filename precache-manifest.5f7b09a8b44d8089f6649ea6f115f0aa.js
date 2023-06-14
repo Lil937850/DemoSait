d9eac2a334f6b59823794c76f5781983376bb47c
Lil937850 <lv937850@gmail.com>
2023-06-14 22:28:19 +0300

    Updates

---
+++ precache-manifest.5f7b09a8b44d8089f6649ea6f115f0aa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "4583a7ea3dab4f39c1b4",
+    "url": "/DemoSait/static/css/main.caec9833.chunk.css"
+  },
+  {
+    "revision": "4583a7ea3dab4f39c1b4",
+    "url": "/DemoSait/static/js/main.4583a7ea.chunk.js"
+  },
+  {
+    "revision": "b322c1d33cac28316582",
+    "url": "/DemoSait/static/css/1.39a50e1a.chunk.css"
+  },
+  {
+    "revision": "b322c1d33cac28316582",
+    "url": "/DemoSait/static/js/1.b322c1d3.chunk.js"
+  },
+  {
+    "revision": "a8e94c6840e631fbf6a5",
+    "url": "/DemoSait/static/css/2.f88993e9.chunk.css"
+  },
+  {
+    "revision": "a8e94c6840e631fbf6a5",
+    "url": "/DemoSait/static/js/2.a8e94c68.chunk.js"
+  },
+  {
+    "revision": "a0f34314ee31c0644a53",
+    "url": "/DemoSait/static/js/3.a0f34314.chunk.js"
+  },
+  {
+    "revision": "a80ac260c3ee5a094d7a",
+    "url": "/DemoSait/static/js/4.a80ac260.chunk.js"
+  },
+  {
+    "revision": "7e498a914416a6d22732",
+    "url": "/DemoSait/static/js/runtime~main.7e498a91.js"
+  },
+  {
+    "revision": "638dca092944582378678cc684af6c7f",
+    "url": "/DemoSait/static/media/user.638dca09.png"
+  },
+  {
+    "revision": "16f1ab2a01c7aab8492cd150237b8fcd",
+    "url": "/DemoSait/static/media/preloader1.16f1ab2a.svg"
+  },
+  {
+    "revision": "6146df0a0f6be5efb3d47cf7c0fb666d",
+    "url": "/DemoSait/index.html"
+  }
+];
